986f7348d0afa8f29a2465e0c3d54b6c7a523ebd
byynaryy <endeki@gmail.com>
2020-12-31 09:54:34 +0100

    Generate the module account

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AccountModule } from './account/account.module';
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/account', { useNewUrlParser: true })
+    MongooseModule.forRoot('mongodb://localhost/account', { useNewUrlParser: true }),
+    AccountModule
   ],
   controllers: [AppController],
   providers: [AppService],
