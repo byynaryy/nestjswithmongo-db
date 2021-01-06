d72effd6207322571d5e061ee2711773030f5ac8
byynaryy <endeki@gmail.com>
2021-01-06 23:50:23 +0100

    Fetch all accounts using the account service

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -2,5 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AccountService {
-
+    constructor(@InjectModel('Account') private readonly accountModel: Model<Account>) { }
+    // fetch all accounts
+    async getAllAccount(): Promise<Account[]> {
+        const accounts = await this.accountModel.find().exec();
+        return accounts;
+    }
 }
