be85cf8b12bb7567dc72ce491fd050fd979781b5
byynaryy <endeki@gmail.com>
2021-01-23 21:43:24 +0100

    Post a single account using the account service

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -1,18 +1,30 @@
+// ./src/account/account.service.ts
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Account } from './interfaces/account.interface';
+import { createAccountDTO } from './dto/create-account.dto';
+
 
 @Injectable()
 export class AccountService {
     constructor(@InjectModel('Account') private readonly accountModel: Model<Account>) { }
+
     // fetch all accounts
     async getAllAccount(): Promise<Account[]> {
         const accounts = await this.accountModel.find().exec();
         return accounts;
     }
-    
+
     // Get a single account
     async getAccount(accountID): Promise<Account> {
         const account = await this.accountModel.findById(accountID).exec();
         return account;
     }
 
+    // Post a single account
+    async addAccount(createAccountDTO:Create): : Promise<Account> {
+        const newAccount = await this.accountModel(createAccountDTO);
+        return newAccount.save();
+    }
 }
